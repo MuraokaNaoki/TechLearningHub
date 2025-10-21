d0e4ab01106147d374163c6bf4684e30897bb08d
村岡直樹 <muraokanaoki@niijimaminaminoMacBook-Pro.local>
2025-10-22 06:25:31 +0900

    Add Spring Boot project files and initial setup

---
+++ src/main/java/com/example/TechLearningHub/TechLearningHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechLearningHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLearningHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLearningHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TechLearningHub/TechLearningHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechLearningHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLearningHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
